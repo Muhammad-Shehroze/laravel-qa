9a5b2db7999a68125502c3e5aebcb8b046c6675c
Shehroze-Kamboh <shehroze511@gmail.com>
2021-03-03 08:52:27 +0500

    Lesson 5 Completed

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,19 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header"><h1>All Questions</h1></div>
+                <div class="card-header">All Questions</div>
+
                 <div class="card-body">
                    @foreach ($questions as $question)
                         <div class="media">
                             <div class="media-body">
-                                <h3 class="mt-0">{{ $question->title }}</h3>
-                                {{ \Illuminate\Support\Str::limit($question->body, 250) }}
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by 
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
+                                {{ Str::limit($question->body, 250) }}
                             </div>                        
                         </div>
                         <hr>
                    @endforeach
 
-                    <div style="text-align: center" class="mx-auto">
+                    <div class="mx-auto">
                         {{ $questions->links() }}
                     </div>
                 </div>
