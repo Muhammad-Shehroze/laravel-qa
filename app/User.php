c21c68557deffe0280f09d4321afd400ef4ba6ff
Shehroze-Kamboh <shehroze511@gmail.com>
2021-03-04 11:15:35 +0500

    Lesson-15 Completed

--- app/User.php
+++ app/User.php
@@ -49,5 +49,10 @@ class User extends Authenticatable
         // return route('questions.show', $this->id);
         return '#';
     }
-
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 30;
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 }
